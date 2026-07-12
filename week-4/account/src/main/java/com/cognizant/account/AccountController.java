c5e7f53c94660c7d3ece1faa6e3aadc327839b46
georgin <georginsomichan@gmail.com>
2026-07-12 21:14:08 +0530

    microservices

---
+++ week-4/account/src/main/java/com/cognizant/account/AccountController.java
@@ -0,0 +1,15 @@
+package com.cognizant.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccountDetails(@PathVariable String number) {
+        // Returning the dummy response as specified in the requirements
+        return new Account("00987987973432", "savings", 234343);
+    }
+}
